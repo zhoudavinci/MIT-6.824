0234c473d2cf3c9196ef4f7be2080be53cf8f9ef
zhoudavinci <zhoudavinci@gmail.com>
2019-07-07 22:56:16 +0800

    lab1, part 3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+  "fmt"
+  "sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +33,57 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+  switch phase {
+  case mapPhase:
+    // Be sure of all the tasks has been done.
+    wg := sync.WaitGroup{}
+    wg.Add(ntasks)
+    for i := 0; i < ntasks; i++ {
+      fmt.Println("map task id: ", i)
+      taskArgs := DoTaskArgs{jobName, mapFiles[i], mapPhase, i, n_other}
+      // get worker's address, use channel for Goroutine func with return value
+      reg := make(chan string)
+      go getRegister(registerChan, reg)
+      register := <-reg
+
+      // call rpc for tasks with Goroutine, meantime, pushing the worker's address into the channel after the task has been done.
+      go callRpc(register, registerChan, taskArgs, &wg)
+    }
+    wg.Wait()
+  case reducePhase:
+    wg := sync.WaitGroup{}
+    wg.Add(ntasks)
+    for i := 0; i < ntasks; i++ {
+      fmt.Println("reduce task id: ", i)
+      taskArgs := DoTaskArgs{jobName, "", reducePhase, i, n_other}
+
+      reg := make(chan string)
+      go getRegister(registerChan, reg)
+      register := <-reg
+
+      go callRpc(register, registerChan, taskArgs, &wg)
+    }
+    wg.Wait()
+  }
+
 	fmt.Printf("Schedule: %v done\n", phase)
+}
+
+// use call() to do map/reduce task, and push the worker's address into channel for next map/reduce task. the wg should be a pointer, otherwise the process would be dead-lock status.
+func callRpc(register string, registerChan chan string, taskArgs DoTaskArgs, wg *sync.WaitGroup) {
+  ok := call(register, "Worker.DoTask", taskArgs, nil)
+  if (ok == false) {
+    fmt.Println("map/reduce task error.")
+  }
+  wg.Done()
+  setRegister(registerChan, register)
+}
+
+func getRegister(register chan string, res chan string) {
+  tmp := <-register
+  res <- tmp
+}
+
+func setRegister(register chan string, res string) {
+  register <- res
 }
